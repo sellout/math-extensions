25a955af0f6ce58883067260659b882d442953f1
Greg Pfeil <greg@technomadic.org>
2011-02-23 22:52:28 -0500

    A couple more tests.

--- tests/numeric-sets.lisp
+++ tests/numeric-sets.lisp
@@ -5,5 +5,11 @@
 (in-suite numeric-sets-tests)
 
 (test should-create-set-from-+-
-  (is (= (+- 7)
-         (make-instance 'enumerated-set :initial-contents '(-7 7)))))
+  (is (= (make-instance 'enumerated-set :initial-contents '(-7 7))
+         (+- 7))))
+
+(test should-collapse-set-with-abs
+  (is (= 7 (abs (+- 7)))))
+
+(test should-add-mixed-values
+  (is (= (make-set 4 6) (+ 3 (make-set 1 3)))))
